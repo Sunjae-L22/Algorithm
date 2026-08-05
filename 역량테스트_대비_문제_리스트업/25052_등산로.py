642231a84af5a6c265e2b782861563d756d8f33d
sunjae <leeseonjae0111@gmail.com>
2026-08-05 09:29:55 +0900

    등산로 완

---
+++ 역량테스트_대비_문제_리스트업/25052_등산로.py
@@ -0,0 +1,45 @@
+T = int(input())
+
+
+# 영역 정보와 시작점이 주어졌을 때, 최장경로를 구하는 함수
+def cal_path_len(area, start_col, start_row):
+    # 상하좌우
+    dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    path_len = 1
+    row, col = start_row, start_col
+    real_row, real_col = 0, 0
+    while True:
+        now_height = area[col][row]
+        switch = False
+        for i in range(4):
+            nrow = row + dir[i][1]
+            ncol = col + dir[i][0]
+            if 0 <= nrow < N and 0 <= ncol < N:
+                if area[ncol][nrow] < now_height:
+                    switch = True
+                    now_height = area[ncol][nrow]
+                    real_row = nrow
+                    real_col = ncol
+
+        if switch:
+            col = real_col
+            row = real_row
+            path_len += 1
+        else:
+            break
+    return path_len
+
+
+
+for test_case in range(1, T+1):
+    N = int(input())
+    area = []
+    for i in range(N):
+        area.append(list(map(int, input().split())))
+
+    # N제곱만큼 경로 존재, 그중 가장 긴 경로 찾기
+    path_length = [0] * (N**2)
+    for i in range(N):
+        for j in range(N):
+            path_length[i*N+j] = cal_path_len(area, i, j)
+    print(f"#{test_case} {max(path_length)}")
