e1248c7261d775a22765b0f67ce5da9c3140232b
SunjaeLee <73410457+Sunjae-L22@users.noreply.github.com>
2026-09-03 08:53:25 +0900

    [Passed] 4가지 관계연산자 (49ms, 16MB)

---
+++ codetree/trail1/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,17 @@
+A, B = map(int, input().split())
+
+if A > B:
+    print(1)
+    print(1)
+    print(0)
+    print(0)
+elif A == B:
+    print(1)
+    print(0)
+    print(1)
+    print(0)
+else:
+    print(0)
+    print(0)
+    print(1)
+    print(1)
